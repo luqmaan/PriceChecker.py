10cfb816ac7090c1bb6473d9e91c405c36fe9fb6
Luqmaan <ldawoodjee@gmail.com>
2013-03-07 15:59:46 -0500

    Visual refinements to the extension

--- background.js
+++ background.js
@@ -1,17 +1,40 @@
+function loadFiles(i, files, tab) {
+    setTimeout(function() {
+        if (i === files.length)
+            return;
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    console.log(tab);
-    console.log("clickedweofijewoifj");
-    chrome.tabs.executeScript(tab.id, {
-        file: "jquery.min.js"
-    }, function() {
-        chrome.tabs.insertCSS(tab.id, {
-            file: "marklet.css"
-        }, function() {
+        var fname = files[i];
+        var typeregex = /\.([0-9a-z]+)(?:[\?#]|$)/i;
+        var ftype = fname.match(typeregex);
+
+        console.log(Date.now() + " " + fname);
+
+        switch (ftype[1]) {
+        case "css":
+            chrome.tabs.insertCSS(tab.id, {
+                file: fname
+            }, loadFiles(i+1, files, tab));
+            break;
+        case "js":
             chrome.tabs.executeScript(tab.id, {
-                file: "marklet.js"
-            });
-        });
-    });
+                file: fname
+            }, loadFiles(i+1, files, tab));
+            break;
+        default:
+            console.error("Unexpected file extension: " + ftype);
+            break;
+        }
+    }, 300);
+
+
+}
+
+// Called when the user clicks on the extension icon.
+chrome.browserAction.onClicked.addListener(function(tab) {
+
+    var files = ["jquery.min.js", "marklet.css",  "jquery.xpath.min.js", "marklet.js"];
+
+    loadFiles(0, files, tab);
+
 });
+
--- marklet.js
+++ marklet.js
@@ -1,42 +1,46 @@
-// javascript:c=document.createElement('script');c.type='text/javascript';c.src='file://localhost/Users/lolcat/dev/PriceChecker.py/marklet.js?ts='+new Date().getTime();document.getElementsByTagName('HEAD')[0].appendChild(c);
+(function($) {
 
-// prevent duplicate loads
+	// display simple floating UI with data
+	$("body *").on("mouseover.marklet", function(el) {
+		$(el.target).addClass("marklet-active");
+	});
+	$("body *").on("mouseout.marklet", function(el) {
+		$(el.target).removeClass("marklet-active");
+	});
 
-// getXPath
+	$("body *").on('click.marklet', function(el) {
 
-// display simple floating UI with data
-$("body *").on("mouseover", function(el) {
-	$(el.target).addClass("marklet-active");
-});
-$("body *").on("mouseout", function(el) {
-	$(el.target).removeClass("marklet-active");
-});
+		$(this).fadeOut(500, function() {$(this).fadeIn(200);});
+		$(this).removeClass("marklet-active");
 
-$("body *").on('click', function(el) {
-	var x = [],
-		t = el.target;
+		var x = [],
+			t = el.target;
 
-	while (t !== undefined && t.nodeName !== "HTML") {
-		var node = t.nodeName;
-		// XXX as soon as we find a node with an id or class, we can probably break to save 
-		// unnecessary paths...
-		if (t.id !== "") {
-			node += "#" + t.id;
-		} else if (t.className !== "") {
-			node += "." + t.className; // XXX multiple classes? replace(" ", ",");	
+		while (t !== undefined && t.nodeName !== "HTML") {
+			var node = t.nodeName;
+			// XXX as soon as we find a node with an id or class, we can probably break to save 
+			// unnecessary paths...
+			if (t.id !== "") {
+				node += "#" + t.id;
+			} else if (t.className !== "") {
+				node += "." + t.className; // XXX multiple classes? replace(" ", ",");	
+			}
+			x[x.length] = node;
+			t = t.parentNode;
 		}
-		x[x.length] = node;
-		t = t.parentNode;
-	}
 
-	var xpath = x.reverse().join(" > ");
-	console.log(xpath);
+		var xpath = x.reverse().join(" > ");
+		console.log(xpath);
 
-	// maybe we shouldnt override their event handlers? 
-	event.preventDefault();
+		// maybe we shouldnt override their event handlers? 
+		event.preventDefault();
 
-	return false;
-});
+		$("body *").off("click.marklet");
+		$("body *").off("mouseover.marklet");
+		$("body *").off("mouseout.marklet");
+
+		return false;
+	});
 
 
-// });
+})(jQuery);
