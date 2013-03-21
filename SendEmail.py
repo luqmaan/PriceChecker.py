f6eed22bc90a28b2d8d160d81285cb9958cfd007
shano jacob <sjacob@mai.usf.edu>
2013-03-21 19:32:39 -0400

    Email Notification using Mandrill

--- SendEmail.py
+++ SendEmail.py
@@ -1,22 +1,38 @@
 #Email Notification
 
+import os
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-def SendEmail(toaddr,msg):
-    fromaddr = 'PriceChecker12345@gmail.com'   
+def SendEmail(to):
+    msg = MIMEMultipart('alternative')
 
-    username = 'PriceChecker12345@gmail.com'
-    password = 'forpythonclass'
+    msg['Subject'] = "Hello!"
+    msg['From']    = "info@pychecker.com" 
+    msg['To']      = to
 
-    server = smtplib.SMTP('smtp.gmail.com:587')
-    server.starttls()
-    server.login(username,password)
-    server.sendmail(fromaddr, toaddr, msg)
-    server.quit()
+    text = "Test message"
+    part1 = MIMEText(text, 'plain')
 
-toaddr='shano142@yahoo.com'
-msg='Hellooooo'
-SendEmail(toaddr,msg)
+    html = "<i>Hai this is a test email</i>"
+    part2 = MIMEText(html, 'html')
+
+    username = os.environ['MANDRILL_USERNAME']
+    password = os.environ['MANDRILL_PASSWORD']
+
+    msg.attach(part1)
+    msg.attach(part2)
+
+    s = smtplib.SMTP('smtp.mandrillapp.com', 25)
+    s.login(username, password)
+
+    s.sendmail(msg['From'], msg['To'], msg.as_string())
+
+    s.quit()
+
+toaddr = '' # To address
+SendEmail(toaddr)
 
 
 
