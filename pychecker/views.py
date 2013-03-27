a49a30e9cbddb30e006bc97981d7bfd228ee55cc
Luqmaan <ldawoodjee@gmail.com>
2013-03-27 10:36:52 -0400

    Working on dashbaord, lots to do still

--- pychecker/views.py
+++ pychecker/views.py
@@ -1,6 +1,6 @@
 from pychecker import app
 from pychecker.database import db_session
-from pychecker.models import User
+from pychecker import models
 from pychecker import LoginManager
 from pychecker import forms
 from flask import render_template
@@ -29,11 +29,11 @@ def index():
 def register():
     error = None
     if request.method == 'POST':
-        u = User(request.form['username'],
-                 request.form['password'],
-                 request.form['email'],
-                 request.form['phone'],
-                 request.form['twitter'])
+	u = models.User(request.form['username'],
+			request.form['password'],
+			request.form['email'],
+			request.form['phone'],
+			request.form['twitter'])
         db_session.add(u)
         try:
             db_session.commit()
@@ -52,7 +52,7 @@ def register():
 @app.route('/users')
 def users():
     error = None
-    db_users = User.query.all()
+    db_users = models.User.query.all()
     return render_template('users.html', users=db_users, error=error)
 
 
@@ -96,17 +96,36 @@ def dashboard():
 
 @app.route('/product/', methods=['GET', 'POST'])
 def product():
+    message = None
+    products = current_user.following
     form = forms.ProductForm()
     if request.method == 'GET':
 	return render_template('dashboard.html', form=None)
     if request.method == 'POST':
 	if form.validate():
-	    message = "Your product has been succesfully added."
-	    return render_template('dashboard.html', message=message)
+	    new_product = models.Product(user_id=current_user.get_id(),
+					 name="Test name",
+					 url=form.url,
+					 currentPrice="Test price",
+					 notifyPrice=form.notify_price)
+	    db_session.add(new_product)
+	    try:
+		db_session.commit()
+	    except IntegrityError, e:
+		# this shouldn't happen, should get rid of this
+		message = "Sorry, the product/username " + request.form['username'] + " is already taken."
+	    except Exception, e:
+		message = e
+	    else:
+		message = "Your product has been succesfully added."
+	    return render_template('dashboard.html',
+				   message=message,
+				   products=products,
+				   user=user)
 	else:
-	    return render_template('dashboard.html')
+	    return render_template('dashboard.html', form=form)
     else:
-	return 'add new product or list all products'
+	return 'An unknown error has occurred.'
 
 
 @app.route('/product/<int:product_id>')
